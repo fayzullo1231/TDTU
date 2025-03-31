a0e270c7964d8eae50d9a3588f7a380eb0c1addd
fayzullo1231 <fayzullodev@gmail.com>
2025-04-01 01:20:05 +0500

    first step

---
+++ assets/main/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
