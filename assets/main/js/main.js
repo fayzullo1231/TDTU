eacaf407f0d8f3dd388ccf5c949d97111231e608
fayzullo1231 <fayzullodev@gmail.com>
2025-04-01 19:57:52 +0500

    Home done

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -16,3 +16,28 @@ $(document).ready(function () {
         ]
     });
 });
+
+const imageContainer = document.querySelector('.image-container');
+const prevButton = document.querySelector('.carousel-btn.prev');
+const nextButton = document.querySelector('.carousel-btn.next');
+const images = document.querySelectorAll('.image-container img');
+const imageWidth = images[0].clientWidth;
+const visibleImages = 3;
+let currentIndex = 0;
+
+imageContainer.style.width = `${imageWidth * images.length}px`;
+
+function updateImageContainer() {
+    let translateX = -currentIndex * imageWidth;
+    imageContainer.style.transform = `translateX(${translateX}px)`;
+}
+
+nextButton.addEventListener('click', () => {
+    currentIndex = Math.min(currentIndex + 1, images.length - visibleImages);
+    updateImageContainer();
+});
+
+prevButton.addEventListener('click', () => {
+    currentIndex = Math.max(currentIndex - 1, 0);
+    updateImageContainer();
+});
